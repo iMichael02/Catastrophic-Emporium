96c7f205651387ce69597d2d99781daf25627442
iMichael02 <btm.qtv@gmail.com>
2023-04-12 22:26:37 +0700

    responsive fix 2

---
+++ asset/js/search.js
@@ -0,0 +1,10 @@
+function openSearch() {
+    var searchBar = document.getElementById("mobile-search-bar");
+    searchBar.style.display = "block";
+    var navbar = document.getElementById("topnav-mobile");
+    navbar.style.display = "none";
+}
+function closeSearch() {
+    var searchBar = document.getElementById("mobile-search-bar");
+    searchBar.style.display = "none";
+}
