8c5901a2fcd78ea4e7036fadac6d0a75b741f592
iMichael02 <btm.qtv@gmail.com>
2023-04-12 10:27:36 +0700

    responsive fix 1

---
+++ asset/js/navbar.js
@@ -0,0 +1,8 @@
+function showNavItem() {
+  var x = document.getElementById("topnav-mobile");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
